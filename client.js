eb3f59535e524140951847e710fe93457e3bac63
kdh7037 <kdh7037@naver.com>
2018-12-01 23:45:42 +0900

    asdf

---
+++ client.js
@@ -0,0 +1,82 @@
+var lis = 0;
+
+function createHiddenInput(name, value){
+  var temp=document.createElement("input");
+  temp.setAttribute("type","hidden");
+  temp.setAttribute("name",name);
+        temp.setAttribute("value",value);
+        
+
+  return temp;
+}
+
+    jQuery(function ($) {
+        $(this).on("click", "#btn-new-list", function () {
+            $("#newlist").addClass('d-none');
+            var value = $("#listname").val();
+            var temp = $("#mylist").clone().removeClass('d-none').attr("id","").attr("data-listindex",lis+1);
+            lis++;
+            temp.appendTo("#listform");
+            temp.find(".listtitle").val(value);
+
+              var form=document.createElement("form");
+            form.setAttribute("method","post");
+            form.setAttribute("action","/list_control.php");
+
+              var idx=createHiddenInput("message","add_list");
+            form.appendChild(idx);
+            
+            document.body.appendChild(form);
+            form.submit();
+        });
+    });
+    $(document).ready(function () {
+
+        $('#listform')
+        .on('click', 'button.btn-adjust-name', function () {
+            $('#adjust').modal('show');
+        })
+
+        .on('click', 'span', function () {
+            $('#myModal').modal('show');
+        })
+
+        .on("click", "#btn-remove-card", function () {
+            var temp = $("#mycard").clone().removeClass('d-none').attr("id","");
+            $(this).parent().find(".card-body").first().append(temp);
+        })
+
+        $("#adjust").on('click', '.btn-adj', function () {
+            var adj= $(".title").val();
+            $("#mycard").find("span").val(adj);
+        })
+
+        $("#btn-add-list").on("click", function () {
+            var temp = $("#newlist").removeClass('d-none');
+            temp.appendTo("#listform");
+        })
+    });
+    
+    jQuery(function ($) {
+        $(this).on("click", "#btn-add-card", function () {
+            var temp = $("#mycard").clone().removeClass('d-none').attr("id","");
+            $(this).parent().find(".card-body").first().append(temp);
+
+            var form=document.createElement("form");
+            form.setAttribute("method","post");
+            form.setAttribute("action","/add-list.php");
+
+            var listIdx=temp.parent().parent().getAttribute("data-listindex");
+            var parentList=createHiddenInput("list",listIdx);
+            form.appendchild(parentList);
+
+            document.body.appendChild(form);
+            form.submit();
+        });
+    });
+    jQuery(function ($) {
+        $(document).on("click", "#btn-add-card", function () {
+            var temp = $("#mycard").clone().removeClass('d-none').attr("id","");
+            $(this).parent().find(".card-body").first().append(temp);
+        });
+    }); 
--- main.php
+++ main.php
@@ -8,93 +8,9 @@
  	<link rel="stylesheet" href="css/css.css">
   <link rel="shortcut icon" href="images/favicon.ico" type="image/favicon.ico"/>
   <link rel="icon" href="images/favicon.ico" type="image/favicon.ico"/>
- 	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="client.js"></script>
  	<script src="css/bootstrap/js/bootstrap.bundle.js"></script>
-
-  <script>
-		var num = 0;
-
-    function createHiddenInput(name, value){
-      var temp=document.createElement("input");
-      temp.setAttribute("type","hidden");
-      temp.setAttribute("name",name);
-			temp.setAttribute("value",value);
-			
-
-      return temp;
-    }
-
-		jQuery(function ($) {
-			$(this).on("click", "#btn-new-list", function () {
-				$("#newlist").addClass('d-none');
-				var value = $("#listname").val();
-        		var temp = $("#mylist").clone().removeClass('d-none').attr("id","").attr("data-listindex",num+1);
-				num++;
-				temp.appendTo("#listform");
-				temp.find(".listtitle").val(value);
-
-      			var form=document.createElement("form");
-    			form.setAttribute("method","post");
-    			form.setAttribute("action","/list_control.php");
-
-      			var idx=createHiddenInput("message","add_list");
-				form.appendChild(idx);
-				
-				document.body.appendChild(form);
-				form.submit();
-			});
-		});
-		$(document).ready(function () {
-
-			$('#listform')
-			.on('click', 'button.btn-adjust-name', function () {
-				$('#adjust').modal('show');
-			})
-
-			.on('click', 'span', function () {
-				$('#myModal').modal('show');
-			})
-
-			.on("click", "#btn-remove-card", function () {
-				var temp = $("#mycard").clone().removeClass('d-none').attr("id","");
-				$(this).parent().find(".card-body").first().append(temp);
-			})
-
-			$('.btn-adj').on("click", function () {
-				var adj= $(".title").val();
-				$("#mycard").find("span").val(adj);
-			})
-
-			$("#btn-add-list").on("click", function () {
-				var temp = $("#newlist").removeClass('d-none');
-				temp.appendTo("#listform");
-			})
-		});
-		
-		jQuery(function ($) {
-			$(this).on("click", "#btn-add-card", function () {
-				var temp = $("#mycard").clone().removeClass('d-none').attr("id","");
-				$(this).parent().find(".card-body").first().append(temp);
-
-				var form=document.createElement("form");
-				form.setAttribute("method","post");
-				form.setAttribute("action","/add-list.php");
-
-				var listIdx=temp.parent().parent().getAttribute("data-listindex");
-				var parentList=createHiddenInput("list",listIdx);
-				form.appendchild(parentList);
-
-				document.body.appendChild(form);
-				form.submit();
-			});
-		});
-		jQuery(function ($) {
-			$(document).on("click", "#btn-add-card", function () {
-				var temp = $("#mycard").clone().removeClass('d-none').attr("id","");
-				$(this).parent().find(".card-body").first().append(temp);
-			});
-		}); 
-	</script>
  </head>
  
 
