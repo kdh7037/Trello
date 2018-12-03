8f5a2cfcc4de8d3e13ef3ec70272f255994f6679
ldcldc <45259085+ldcldc@users.noreply.github.com>
2018-12-03 19:20:07 +0900

    Update list_control.php

--- list_control.php
+++ list_control.php
@@ -8,22 +8,23 @@ switch ($command[0]) {
 case "add": 							//add\list\name
 	for($i=3; $i<count($command); $i++)			//이름에 \가 있을시 분리된 name 복구
 	$command[2].="\\\\$command[$i]";
-	
-	$query = "select list_id from list 			//마지막 리스트 id 추출(=row[0])
+								//마지막 리스트 id 추출(=row[0])
+	$query = "select list_id from list
 		where link_right=0";
 	$result = mysqli_query($con, $query);
 	$row = mysqli_fetch_row($result);
-	
-	$query = "insert into list (list)			//리스트 추가
+								//리스트 추가
+	$query = "insert into list (list)
 		values ('$command[2]')";
 	mysqli_query( $con, $query );
 	
 	if ($row[0] != "") {					//리스트가 1개이상 있었을때
-		$query = "select max(list_id) from list";	//추가한 리스트 id 추출(=id[0])
+								//추가한 리스트 id 추출(=id[0])
+		$query = "select max(list_id) from list";
 		$result = mysqli_query( $con, $query );
 		$id = mysqli_fetch_row($result);
-		
-		$query = "update list 				//추가한 리스트를 마지막 리스트 뒤로 이동
+								//추가한 리스트를 마지막 리스트 뒤로 이동
+		$query = "update list 				
 			set link_right = $id[0] 
 			where list_id = $row[0]";
 		mysqli_query( $con, $query );
@@ -34,16 +35,17 @@ case "add": 							//add\list\name
 	}
 	break;
 case "delete":							//delete\list_index
-	$query = "select link_left, link_right 			//삭제할 리스트 양옆 리스트 id 추출(=link[])
+								//삭제할 리스트 양옆 리스트 id 추출(=link[])
+	$query = "select link_left, link_right
 		from list where list_id = $command[1]";
 	$result = mysqli_query($con, $query);
 	$link = mysqli_fetch_row($result);
-	
-	$query = "delete from list 				//리스트 삭제
+								//리스트 삭제
+	$query = "delete from list
 		where list_id ='$command[1]'";
 	mysqli_query( $con, $query );
-	
-	$query = "update list 					//삭제된 리스트 양옆 리스트 연결
+								//삭제된 리스트 양옆 리스트 연결
+	$query = "update list
 		set link_right = $link[1] 
 		where link_right = $command[1]";
 	mysqli_query( $con, $query );
@@ -65,12 +67,13 @@ case "modify":
 	else {							//modify\list_place\list_index\list_left
 		if($command[2]==$command[3]) break;
 		$left_id = $command[3];				//left_id=list_left
-		
-		$query = "select link_left, link_right 		//이동시킬 리스트 양옆 리스트 id 추출(=link[])
+								//이동시킬 리스트 양옆 리스트 id 추출(=link[])
+		$query = "select link_left, link_right
 			from list where list_id = $command[2]";
 		$result = mysqli_query($con, $query);
 		$link = mysqli_fetch_row($result);
-		$query = "update list				//양옆 리스트 연결
+								//양옆 리스트 연결
+		$query = "update list
 			set link_right = '$link[1]'
 			where list_id ='$link[0]'";
 		mysqli_query( $con, $query );
@@ -80,12 +83,13 @@ case "modify":
 		mysqli_query( $con, $query );
 						
 		if($left_id == '0') {				//list_left = 0
-			$query = "select list_id		//첫번째 리스트 id 추출(=first_id[0])
+								//첫번째 리스트 id 추출(=first_id[0])
+			$query = "select list_id
 				from list where link_left = '0'";
 			$result = mysqli_query($con, $query);
 			$first_id = mysqli_fetch_row($result);
-			
-			$query = "update list			//이동시킬 리스트를 첫번째 리스트 왼쪽으로 이동
+								//이동시킬 리스트를 첫번째 리스트 왼쪽으로 이동
+			$query = "update list
 				set link_right = '$first_id[0]'
 				where list_id ='$command[2]'";
 			mysqli_query( $con, $query );
@@ -99,12 +103,13 @@ case "modify":
 			mysqli_query( $con, $query );
 		}
 		else {						//list_left > 0
-		$query = "select link_right			//list_left의 오른쪽 리스트 id 추출(=right_id[0])
+								//list_left의 오른쪽 리스트 id 추출(=right_id[0])
+		$query = "select link_right
 			from list where list_id = $command[3]";
 		$result = mysqli_query($con, $query);
 		$right_id = mysqli_fetch_row($result);
-			
-		$query = "update list				//이동시킬 리스트를 리스트 right_id[0], left_id사이로 이동
+								//이동시킬 리스트를 리스트 right_id[0], left_id사이로 이동
+		$query = "update list
 			set link_right = '$command[2]'
 			where list_id ='$left_id'";
 		mysqli_query( $con, $query );
@@ -128,7 +133,8 @@ default:
 }
 
 mysqli_close( $con );
-echo("								//뒤로가기
+								//뒤로가기
+echo("
 	<script>
 	history.go(-1);
 	</script>
