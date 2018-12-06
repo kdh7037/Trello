70f1863ab646189eee88bc4e5ab6279a9fc57849
ldcldc <45259085+ldcldc@users.noreply.github.com>
2018-12-06 22:41:20 +0900

    Add files via upload

---
+++ load_card_detail_db.php
@@ -0,0 +1,29 @@
+<?php
+$con = mysqli_connect( "localhost", "root", "qlzkqaqg123" );		//데이터베이스 연결
+mysqli_select_db( $con, "workspace");
+
+$card_id = $_POST[card_detail];
+																	//해당 카드의 이름($card_detail[0]), description($card_detail[1])]) 추출
+$query = "select card, card_description from card 
+					where card_id =$card_id";
+$result = mysqli_query($con, $query);
+$card_detail = mysqli_fetch_row($result);
+																	//$card = $card_detail[0], $card_description = $card_detail[1]
+list($card, $card_description) = $card_detail;
+																	//comment_count = 해당 카드의 댓글 수
+$query = "select * from card 
+				where card_id = $card_id";
+$result = mysqli_query($con, $query);
+$comment_count = mysqli_num_rows($result);
+
+$query = "select mess, date, user_id 
+				from comment order by comment_id asc 
+				where card_id = $card_id";
+$result = mysqli_query($con, $query);
+while($comment_info = mysqli_fetch_row($result)) {
+	//여기에서 프린트 하면 됨 댓글 다 프린트 될때 까지 반복
+	//$comment_info[0] = mess, $comment_info[1] = date, $comment_info[2] = user_id, 
+}
+
+
+?>
