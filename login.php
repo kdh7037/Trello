046dee1a61d741ecc52be109050b2699d3c1a5fe
ldcldc <45259085+ldcldc@users.noreply.github.com>
2018-12-08 02:45:22 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <?php
-													//쿠키에 저장된 이메일이 있을때 $str_value에 저장
+								//쿠키에 저장된 이메일이 있을때 $str_value에 저장
 if($_COOKIE['email']) 
 	$str_value = "value='".$_COOKIE['email']."'";
 	
-if($_POST[pass] == "pass") {					    	//로그인화면을 지나왔을때
-													    //입력받은 값 앞뒤 공백(띄어쓰기) 제거
+if($_POST[pass] == "pass") {					//로그인화면을 지나왔을때
+								//입력받은 값 앞뒤 공백(띄어쓰기) 제거
 	$mem_email = trim($_POST[mem_email]);
 	$mem_password = trim($_POST[mem_password]);
-														//mem_email, mem_password 중 하나라도 없을 시 오류 출력, 뒤로가기
+								//mem_email, mem_password 중 하나라도 없을 시 오류 출력, 뒤로가기
 	if(!$mem_email  || !$mem_password) {
 		echo("
 					<script>
@@ -17,8 +17,8 @@ if($_POST[pass] == "pass") {					    	//로그인화면을 지나왔을때
 				  ");
 		exit;
 	}
-	session_start();											    	//세션 시작
-	$con = mysqli_connect( "localhost", "root", "qlzkqaqg123" );		//데이터베이스 연결
+	session_start();						//세션 시작
+	$con = mysqli_connect( "localhost", "root", "qlzkqaqg123" );	//데이터베이스 연결
 	mysqli_select_db( $con, "workspace");
 																		//입력한 이메일과 비밀번호에 맞는 회원정보 추출(=mem_info[])
 	$query = "select mem_id, mem_name,  mem_email, mem_password from member 
@@ -26,8 +26,8 @@ if($_POST[pass] == "pass") {					    	//로그인화면을 지나왔을때
 	$result = mysqli_query($con, $query);
 	$mem_info = mysqli_fetch_row($result);
 
-	if ($mem_info[0] == "") {								//입력한 이메일과 비밀번호에 맞는 회원정보가 없을때
-															//알림문 출력 후 뒤로가기
+	if ($mem_info[0] == "") {			//입력한 이메일과 비밀번호에 맞는 회원정보가 없을때
+							//알림문 출력 후 뒤로가기
 		echo("
 					<script>
 					window.alert('이메일과 비밀번호를 확인해 주세요');
@@ -36,28 +36,28 @@ if($_POST[pass] == "pass") {					    	//로그인화면을 지나왔을때
 				  ");
 		exit;
 	}
-												//로그인 성공시
-												//$_SESSION['$mem_info'] 에 $mem_info 저장
-												//$_SESSION['$mem_info'][0] = mem_id, [1] = mem_name,
-												//			    	    [2] = mem_email, [3] = mem_password																
+							//로그인 성공시
+							//$_SESSION['$mem_info'] 에 $mem_info 저장
+							//$_SESSION['$mem_info'][0] = mem_id, [1] = mem_name,
+							//			[2] = mem_email, [3] = mem_password																
 	$_SESSION['$mem_info'] = array();
 	$_SESSION['mem_id'] = $mem_info[0];
 	array_push ($_SESSION['$mem_info'], $mem_info[0], $mem_info[1], $mem_info[2], $mem_info[3]);
 
 	$remember = $_POST[remember];
 	
-	if($remember == "remember-me") {		    //Remember me에 체크되있을때
-		$email = $_POST[mem_email];				//입력받은 이메일 쿠키에 저장(10일동안)
-		$time = time() + (60*60*24*10);			//현재시간 + 60(초)*60(분)*24(시)*10(일)
+	if($remember == "remember-me") {		//Remember me에 체크되있을때
+		$email = $_POST[mem_email];		//입력받은 이메일 쿠키에 저장(10일동안)
+		$time = time() + (60*60*24*10);		//현재시간 + 60(초)*60(분)*24(시)*10(일)
 		setcookie("email", $email, $time); 
 		}
-	else 										//체크 안되있을때 쿠키 삭제
+	else 						//체크 안되있을때 쿠키 삭제
 		setcookie("email"); 
 	
-												//main.php로 이동
+							//main.php로 이동
 header("Location:http://127.0.0.1/main.php");
 }
-												//로그인 화면 출력
+							//로그인 화면 출력
 echo("
 <html lang='ko'>
 
